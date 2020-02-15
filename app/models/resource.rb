cc11e75f5176af06abb19e27f9aac8b04296d4e7
Tim Kleier <timkleier@gmail.com>
2020-02-15 10:41:06 -0700

    add platform on seeds

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -17,22 +17,26 @@ class Resource < ApplicationRecord
       {
         title: 'Introduction to Web Development',
         description: 'Introduction to Web Development is a one-stop course that covers all of the tools you’ll need to create websites.',
-        link: 'https://app.pluralsight.com/library/courses/web-development-intro'
+        link: 'https://app.pluralsight.com/library/courses/web-development-intro',
+        platform: 'pluralsight'
       },
       {
         title: 'Computer Fundamentals: Mobile Devices',
         description: 'Any aspiring computer technician must be familiar with mobile devices. From smartphones to tablets and laptops, this course explores the unique hardware and software challenges of portable computers and phones.',
-        link: 'https://app.pluralsight.com/library/courses/computer-fundamentals-mobile-devices'
+        link: 'https://app.pluralsight.com/library/courses/computer-fundamentals-mobile-devices',
+        platform: 'pluralsight'
       },
       {
         title: 'HTML, CSS, and JavaScript: The Big Picture',
         description: "In this course, you'll learn what HTML, CSS, and JavaScript are and how they interact to bring content to your browser.",
-        link: 'https://app.pluralsight.com/library/courses/html-css-javascript-big-picture'
+        link: 'https://app.pluralsight.com/library/courses/html-css-javascript-big-picture',
+        platform: 'pluralsight'
       },
       {
         title: 'JavaScript: From Fundamentals to Functional JS',
         description: "Solidify your knowledge of objects, arrays, and prototypes in JavaScript. Master closures, use higher-order functions/callbacks, and learn underscore.js.",
-        link: 'https://app.pluralsight.com/library/courses/javascript-from-fundamentals-to-functional-js'
+        link: 'https://app.pluralsight.com/library/courses/javascript-from-fundamentals-to-functional-js',
+        platform: 'pluralsight'
       }
     ]
   end
